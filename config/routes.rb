ca1a528a13580cdfd950d40b4c0953fa366f374c
EC2 Default User <ec2-user@ip-172-31-13-223.ap-northeast-1.compute.internal>
2023-03-27 09:36:18 +0000

    [Fin]Bookers2+alpha

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :index, :show, :destroy, :edit, :update]
   resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
     resources :book_comments, only: [:create, :destroy]
-    resources :favorites, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
